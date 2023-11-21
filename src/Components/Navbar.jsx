bdf4adccdd0d2d523173e18319d89cdca3756180
Bright Philip <110538612+brightphi210@users.noreply.github.com>
2023-11-21 10:26:28 -0800

    latest

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,26 +5,26 @@ import proImage from './Images/pro.png'
 
 const Navbar = () => {
   return (
-    <div className='grid grid-cols-4 px-20 py-5 bg-white'>
-        <div className='col-span-3 flex gap-20'>
+    <div className='grid grid-cols-3 px-20 py-5 bg-white items-center'>
+        <div className='col-span-2 flex gap-20'>
             <h2>Algocal</h2>
             <div><input type="text" placeholder='Search for events here' /></div>
         </div>
 
-        <div className='col-span-1 flex gap-20'>
-            <div>
+        <div className='col-span-1 flex gap-20 items-center'>
+            <div className='bg-zinc-200 p-3'>
                 <p>Nigeria</p>
             </div>
 
-            <div className='flex items-end'>
+            <div className='flex items-center bg-zinc-200 p-3'>
                 <IoIosNotificationsOutline />
-                <p>Notification</p>
+                <p className='text-lg'>Notification</p>
             </div>
 
-            <div className='flex items-center'>
+            <div className='flex items-center gap-5'>
                 <img src={proImage} alt="" className='w-8 h-8'/>
-                <p>algoname</p>
-                <FaCaretDown />
+                <p className='flex items-center '>algoname <FaCaretDown /></p>
+                
             </div>
         </div>  
     </div>
