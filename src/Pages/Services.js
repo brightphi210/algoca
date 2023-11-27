79ca9bc6097063f3ec1b70f96ceb33319e46421d
Bright Philip <110538612+brightphi210@users.noreply.github.com>
2023-11-27 07:48:45 -0800

    latest

--- src/Pages/Services.js
+++ src/Pages/Services.js
@@ -2,14 +2,22 @@ import React from 'react'
 import Navbar from '../Components/Navbar'
 import Sidebar from '../Components/Sidebar'
 import ServicesBody from '../Components/Services/ServicesBody'
-
+import { useState } from 'react'
 const Services = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
   return (
     <div className='bg-zinc-100'>
-    <Navbar />
-    <div className='flex '>
-      <Sidebar />
-      <ServicesBody />
+        <Navbar isOpen={menuOpen} toggleMenu={toggleMenu}/>
+        <div className='flex '>
+          {menuOpen && <Sidebar />}
+          <div className='max-sm:hidden'>
+            <Sidebar />
+          </div>
+      {/* <ServicesBody /> */}
     </div>
 </div>
   )
