57b1a05416033623202af67d61d926c488760567
Bright Philip <110538612+brightphi210@users.noreply.github.com>
2023-11-27 07:27:33 -0800

    latest

--- src/Pages/Account.js
+++ src/Pages/Account.js
@@ -3,12 +3,23 @@ import Sidebar from '../Components/Sidebar'
 import Navbar from '../Components/Navbar'
 import AccountBody from '../Components/Account/AccountBody'
 
+import { useState } from 'react'
+
 const Account = () => {
+
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
   return (
     <div className='bg-zinc-100'>
-        <Navbar />
+        <Navbar isOpen={menuOpen} toggleMenu={toggleMenu}/>
         <div className='flex '>
-          <Sidebar />
+          {menuOpen && <Sidebar />}
+          <div className='max-sm:hidden'>
+            <Sidebar />
+          </div>
           <AccountBody />
         </div>
     </div>
