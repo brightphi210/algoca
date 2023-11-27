e11eabf8faa87a25cd9c529a1b7a99d97c7d6940
Bright Philip <110538612+brightphi210@users.noreply.github.com>
2023-11-27 05:23:42 -0800

    latest

--- src/Components/CreateEvent/CreateBody.jsx
+++ src/Components/CreateEvent/CreateBody.jsx
@@ -44,17 +44,17 @@ const CreateBody = () => {
               <p className='pt-14 font-semibold max-sm:pt-7 max-sm:text-sm'> Event Details</p>
 
               {/* ================= Evenet Details ================================== */}
-              <div className='py-6'>
+              <div className='py-6 max-sm:py-3'>
                 <div className='grid grid-cols-3 items-center gap-5 max-sm:block'>
 
                   <div className='col-span-2'>
-                    <label className='text-sm text-zinc-500 pb-3 block max-sm:text-sm max-sm:py-2 max-sm:pt-4'>Event title:</label>
+                    <label className='text-sm text-zinc-500 pb-3 block max-sm:text-xs max-sm:py-2 max-sm:pt-4'>Event title:</label>
                     {/* <input type="text" className='border border-zinc-200 p-4 w-full mt-3 outline-none rounded-xl'/> */}
                     <input type="text"  className="input input-bordered input-lg w-full max-h-14 min-h-fit flex-shrink-0" />
                   </div>
 
                   <div className='col-span-1'>
-                    <label className='text-sm text-zinc-500 pb-3 block max-sm:text-sm max-sm:py-2 max-sm:pt-4'>Price:</label>
+                    <label className='text-sm text-zinc-500 pb-3 block max-sm:text-xs max-sm:py-2 max-sm:pt-4'>Price:</label>
                     {/* <input type="text" className='border border-zinc-200 p-4 w-full mt-3 outline-none rounded-xl'/> */}
                     <input type="text"  className="input input-bordered input-lg w-full max-h-14 min-h-fit flex-shrink-0" />
                   </div>
@@ -62,7 +62,7 @@ const CreateBody = () => {
                 </div>
 
                 <div className='pt-5 max-sm:p-0'>
-                    <label className='text-sm text-zinc-500 pb-3 block max-sm:text-sm max-sm:py-2 max-sm:pt-4'>Description:</label>
+                    <label className='text-sm text-zinc-500 pb-3 block max-sm:text-xs max-sm:py-2 max-sm:pt-4'>Description:</label>
                     <textarea type="text" className='border border-zinc-200 p-4 min-w-full max-w-full mt-3 outline-none rounded-xl'/>
                   </div>
               </div>
@@ -72,36 +72,43 @@ const CreateBody = () => {
 
               {/* ================= Evenet Dates ================================== */}
 
-              <div className='flex gap-14 max-sm:block'>
+              <div className='flex gap-14 max-sm:block w-full'>
                   <div>
                     <p className='max-sm:text-sm'>Date Of Event</p>
                     <div className=" w-full flex gap-3 pt-5 max-sm:pt-3">
 
                       <div>
-                        <p className='text-sm text-zinc-400 pb-2 '>Year:</p>
-                          <select className="select select-bordered">
-                            <option>2023</option>
-                            <option>2024</option>
-                            <option>2025</option>
+                        <p className='text-sm text-zinc-400 pb-2 max-sm:text-xs'>Year:</p>
+                          <select className="select select-bordered max-sm:text-xs ">
+                            <option className='max-sm:text-xs'>2023</option>
+                            <option className='max-sm:text-xs'>2024</option>
+                            <option className='max-sm:text-xs'>2025</option>
                         </select>
                       </div>
 
 
                       <div>
-                        <p className='text-sm text-zinc-400 pb-2 '>Month:</p>
-                        <select className="select select-bordered">
-                          <option>September</option>
-                          <option>October</option>
-                          <option>November</option>
+                        <p className='text-sm text-zinc-400 pb-2 max-sm:text-xs'>Month:</p>
+                        <select className="select select-bordered max-sm:text-xs ">
+                          <option className='max-sm:text-xs'>September</option>
+                          <option className='max-sm:text-xs'>October</option>
+                          <option className='max-sm:text-xs'>November</option>
                         </select>
                       </div>
 
                       <div>
-                        <p className='text-sm text-zinc-400 pb-2 '>Days:</p>
-                        <select className="select select-bordered">
-                          <option>30</option>
-                          <option>31</option>
-                          <option>1</option>
+                        <p className='text-sm text-zinc-400 pb-2 max-sm:text-xs'>Days:</p>
+                        <select className="select select-bordered max-sm:text-xs ">
+                          <option className='max-sm:text-xs'>1</option>
+                          <option className='max-sm:text-xs'>2</option>
+                          <option className='max-sm:text-xs'>3</option>
+                          <option className='max-sm:text-xs'>4</option>
+                          <option className='max-sm:text-xs'>5</option>
+                          <option className='max-sm:text-xs'>6</option>
+                          <option className='max-sm:text-xs'>7</option>
+                          <option className='max-sm:text-xs'>8</option>
+                          <option className='max-sm:text-xs'>9</option>
+                          <option className='max-sm:text-xs'>10</option>
                         </select>
                       </div>
 
@@ -114,27 +121,27 @@ const CreateBody = () => {
                     <div className=" w-full flex gap-3 pt-5 max-sm:pt-3">
 
                       <div>
-                        <p className='text-sm text-zinc-400 pb-2 '>Hours:</p>
+                        <p className='text-sm text-zinc-400 pb-2 max-sm:text-xs'>Hours:</p>
                         <div className='flex items-center gap-3'>
-                          <input type="text" placeholder='00' className='w-14 max-sm:w-20 rounded-lg border border-zinc-300 p-3 px gap-2-4 outline-none'/>
+                          <input type="text" placeholder='00' className='w-14 max-sm:w-16 max-sm:text-xs rounded-lg border border-zinc-300 p-3 px gap-2-4 outline-none'/>
                           <div>:</div>
                         </div>
                       </div>
                     
 
                       <div>
-                        <p className='text-sm text-zinc-400 pb-2 '>Mins:</p>
+                        <p className='text-sm text-zinc-400 pb-2 max-sm:text-xs'>Mins:</p>
                         <div className='flex items-center gap-3'>
-                          <input type="text" placeholder='00' className='w-14 max-sm:w-20 rounded-lg border border-zinc-300 p-3 px gap-2-4 outline-none'/>
+                          <input type="text" placeholder='00' className='w-14 max-sm:w-16 max-sm:text-xs rounded-lg border border-zinc-300 p-3 px gap-2-4 outline-none'/>
                           <div>:</div>
                         </div>
                       </div>
 
                       <div>
-                        <p className='text-sm text-zinc-400 pb-2 '>am-pm:</p>
-                        <select className="select select-bordered">
-                          <option>am</option>
-                          <option>pm</option>
+                        <p className='text-sm text-zinc-400 pb-2 max-sm:text-xs'>am-pm:</p>
+                        <select className="select select-bordered max-sm:text-xs max-sm:h-10 max-sm:min-h-fit">
+                          <option  className='max-sm:text-xs'>am</option>
+                          <option  className='max-sm:text-xs'>pm</option>
                         </select>
                       </div>
 
@@ -148,18 +155,18 @@ const CreateBody = () => {
                       <div>
                           <div className="flex gap-4">
                             <div>
-                              <p className='text-sm text-zinc-400 pb-2 '>Days:</p>
-                              <input type="text" placeholder='00' className='w-14 max-sm:w-20 rounded-lg border border-zinc-300 p-3 px gap-2-4 outline-none'/>
+                              <p className='text-sm text-zinc-400 pb-2 max-sm:text-xs'>Days:</p>
+                              <input type="text" placeholder='00' className='w-14 max-sm:w-16 max-sm:text-xs rounded-lg border border-zinc-300 p-3 px gap-2-4 outline-none'/>
                             </div>
 
                             <div>
-                              <p className='text-sm text-zinc-400 pb-2 '>Hours:</p>
-                              <input type="text" placeholder='00' className='w-14 max-sm:w-20 rounded-lg border border-zinc-300 p-3 px gap-2-4 outline-none'/>
+                              <p className='text-sm text-zinc-400 pb-2 max-sm:text-xs'>Hours:</p>
+                              <input type="text" placeholder='00' className='w-14 max-sm:w-16 max-sm:text-xs rounded-lg border border-zinc-300 p-3 px gap-2-4 outline-none'/>
                             </div>
 
                             <div>
-                              <p className='text-sm text-zinc-400 pb-2 '>Minutes:</p>
-                              <input type="text" placeholder='00' className='w-14 max-sm:w-20 rounded-lg border border-zinc-300 p-3 px gap-2-4 outline-none'/>
+                              <p className='text-sm text-zinc-400 pb-2 max-sm:text-xs'>Minutes:</p>
+                              <input type="text" placeholder='00' className='w-14 max-sm:w-16 max-sm:text-xs rounded-lg border border-zinc-300 p-3 px gap-2-4 outline-none'/>
                             </div>
                         </div>
                       </div>
@@ -179,7 +186,7 @@ const CreateBody = () => {
                   <p className='text-sm text-zinc-600 py-5'>Speakers:</p>
                   <div className='w-fit flex gap-4'>
 
-                    <div className='flex bg-zinc-100 p-2 gap-5 px-6 rounded-xl items-center'>
+                    <div className='flex bg-zinc-100 p-2 gap-5 px-6 max-sm:px-4 rounded-xl items-center'>
                       <img src={proImage} alt="" className='w-12' />
                       <div>
                         <p className='text-sm'>algoname</p>
@@ -190,7 +197,7 @@ const CreateBody = () => {
 
 
                   <div>
-                    <img src={add} alt="" className='w-16 cursor-pointer' onClick={()=>document.getElementById('my_modal_2').showModal()}/>
+                    <img src={add} alt="" className='w-16 cursor-pointer max-sm:w-10' onClick={()=>document.getElementById('my_modal_2').showModal()}/>
                   </div>
 
                   </div>
@@ -199,52 +206,52 @@ const CreateBody = () => {
                 <dialog id="my_modal_2" className="modal">
                   <div className="modal-box">
 
-                    <h3 className="font-bold text-lg pb-5">Select Speaker!</h3>
+                    <h3 className="font-bold text-lg pb-5 max-sm:text-sm">Select Speaker!</h3>
                     <input type="text" placeholder="Search speakers here" className="input input-bordered input-md w-full mb-6" />
 
                     <hr className='border-zinc-100'/>
                     <div className='flex items-center py-3'>
                       <div className='flex gap-3 items-center'>
-                        <img src={proImage} alt="" className='w-10'/>
-                        <p>User Name</p>
+                        <img src={proImage} alt="" className='w-10 max-sm:w-8'/>
+                        <p className='max-sm:text-sm'>User Name</p>
                       </div>
-                        <input type="radio" name="radio-2" className="radio radio-primary ml-auto w-5 h-5 p-0" />
+                        <input type="radio" name="radio-2" className="radio radio-primary ml-auto w-5 h-5 p-0 max-sm:w-3 max-sm:h-3" />
                     </div>
 
                     <hr className='border-zinc-100'/>
 
                     <div className='flex items-center py-3'>
                       <div className='flex gap-3 items-center'>
-                        <img src={proImage} alt="" className='w-10'/>
-                        <p>User Name</p>
+                        <img src={proImage} alt="" className='w-10 max-sm:w-8'/>
+                        <p className='max-sm:text-sm'>User Name</p>
                       </div>
-                        <input type="radio" name="radio-2" className="radio radio-primary ml-auto w-5 h-5 p-0" />
+                        <input type="radio" name="radio-2" className="radio radio-primary ml-auto w-5 h-5 p-0 max-sm:w-3 max-sm:h-3" />
                     </div>
 
                     <hr className='border-zinc-100'/>
 
                     <div className='flex items-center py-3'>
                       <div className='flex gap-3 items-center'>
-                        <img src={proImage} alt="" className='w-10'/>
-                        <p>User Name</p>
+                        <img src={proImage} alt="" className='w-10 max-sm:w-8'/>
+                        <p className='max-sm:text-sm'>User Name</p>
                       </div>
-                        <input type="radio" name="radio-2" className="radio radio-primary ml-auto w-5 h-5 p-0" />
+                        <input type="radio" name="radio-2" className="radio radio-primary ml-auto w-5 h-5 p-0 max-sm:w-3 max-sm:h-3" />
                     </div>
 
                     <hr className='border-zinc-100'/>
 
                     <div className='flex items-center py-3'>
                       <div className='flex gap-3 items-center'>
-                        <img src={proImage} alt="" className='w-10'/>
-                        <p>User Name</p>
+                        <img src={proImage} alt="" className='w-10 max-sm:w-8'/>
+                        <p className='max-sm:text-sm'>User Name</p>
                       </div>
-                        <input type="radio" name="radio-2" className="radio radio-primary ml-auto w-5 h-5 p-0" />
+                        <input type="radio" name="radio-2" className="radio radio-primary ml-auto w-5 h-5 p-0 max-sm:w-3 max-sm:h-3" />
                     </div>
 
                     <hr className='border-zinc-100'/>
 
                     <div className='flex gap-3 pt-5'>
-                      <button className="btn btn-active btn-primary rounded-3xl px-10 bg-blue-800 text-white" >Add</button>
+                      <button className="btn btn-active btn-primary rounded-3xl px-10 bg-blue-800 text-white max-sm:text-sm max-sm:h-10 max-sm:min-h-fit" >Add</button>
                       {/* <button className="btn btn-neutral rounded-3xl px-10 text-zinc-700 bg-blue-800 border-none hover:bg-zinc-200">Add</button> */}
                     </div>
                   
@@ -262,7 +269,7 @@ const CreateBody = () => {
                   <p className='text-sm text-zinc-600 py-5'>Guest Speakers:</p>
                   <div className='flex gap-4 w-fit'>
 
-                    <div className='flex bg-zinc-100 p-2 gap-5 px-6 rounded-xl items-center'>
+                    <div className='flex bg-zinc-100 p-2 gap-5 px-6 max-sm:px-4 rounded-xl items-center'>
                       <img src={proImage} alt="" className='w-12' />
                       <div>
                         <p className='text-sm'>algoname</p>
@@ -272,7 +279,7 @@ const CreateBody = () => {
                     </div>
 
                   <div>
-                    <img src={add} alt="" className='w-16 cursor-pointer'/>
+                    <img src={add} alt="" className='w-16 max-sm:w-10 cursor-pointer' onClick={()=>document.getElementById('my_modal_2').showModal()}/>
                   </div>
 
                   </div>
@@ -286,17 +293,17 @@ const CreateBody = () => {
 
               <div className='pb-5'>
                 <div className='grid grid-cols-4 items-center max-sm:flex gap-3'>
-                  <p className='col-span-3 font-semibold max-sm:text-sm'>Location (Hosting From)</p>
+                  <p className='col-span-3 font-semibold max-sm:text-xs'>Location (Hosting From)</p>
 
                   <div className='flex items-center gap-4 max-sm:gap-2 col-span-1 max-sm:ml-auto'>
-                    <p className='text-sm text-zinc-500'>Virtual Event</p>
+                    <p className='text-xs text-zinc-500 '>Virtual Event</p>
                     <input type="checkbox" className="toggle max-sm:w-10" onClick={open}/>
                   </div>
                 </div>
 
                 {toggle ? 
                 (<>
-                    <div className='flex w-full gap-5 items-center pt-5 max-sm:block'>
+                    <div className='flex w-full gap-5 items-center pt-5 max-sm:block max-sm:pt-3'>
                       <div >
                         <p className='py-3 text-sm max-sm:text-xs'>Country:</p>
                         <select className="select select-bordered select-lg w-full max-h-14 min-h-fit max-sm:text-sm">
@@ -342,7 +349,7 @@ const CreateBody = () => {
                   <>
                     <p className='py-3 text-sm'>Web Link1:</p>
                     <div className='flex items-center gap-4 max-sm:block max-sm:w-full'>
-                        <div className='flex gap-4 items-center'>
+                        <div className='flex gap-4 items-center w-fit'>
                           <input type="text"  className="input input-bordered input-lg w-96 max-h-14 min-h-fit flex-shrink-0 max-sm:w-fit" />
                           {links.length > 1 && (
                             <div>
