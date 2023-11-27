40712977627f96a1033dc30bd7cbf7dd2e2c1fa4
Bright Philip <110538612+brightphi210@users.noreply.github.com>
2023-11-27 06:11:36 -0800

    latest

--- src/Components/Announcement/AnnounceBody.jsx
+++ src/Components/Announcement/AnnounceBody.jsx
@@ -9,24 +9,24 @@ import { FaRegCircle } from "react-icons/fa";
 
 const AnnounceBody = () => {
   return (
-    <div className='pt-36 pl-96 px-20 w-full flex items-start h-screen gap-10'>
-        <div className='bg-white p-10 px-20 rounded-3xl'>
-            <p className='flex items-center gap-4 text-xl font-semibold '><GoDotFill className='text-blue-800'/> Announcements</p>
-            <div className=' h-200 overflow-y-scroll px-10'>
+    <div className='pt-36 pl-96 px-20 w-full flex items-start h-screen gap-10 max-sm:gap-0 max-sm:px-4 max-sm:w-screen max-sm:pt-24 max-sm:h-full max-sm:block'>
+        <div className='bg-white p-10 px-20 rounded-3xl  max-sm:px-3 max-sm:mb-10'>
+            <p className='flex items-center gap-4 text-xl font-semibold max-sm:text-sm max-sm:gap-2'><GoDotFill className='text-blue-800'/> Announcements</p>
+            <div className=' h-200 overflow-y-scroll px-10 max-sm:px-5 '>
 
                 <div className='py-5'>
                     <p className='flex gap-4 items-center font-semibold'><FaRegCalendarAlt className='text-blue-800'/>Event</p>
-                    <p className='text-sm py-5 text-zinc-500'>
+                    <p className='text-sm py-5 text-zinc-500 max-sm:text-xs max-sm:text-justify' >
                         Algorand has set an event for fresh & upcoming devs to participate in the building of a new tech solution, <br />
                         happenning live in Nigeria,Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy
                     </p>
-                    <div className='flex items-center '>
+                    <div className='flex items-center max-sm:block'>
                         <p className='text-sm text-blue-800 font-semibold'>View Event</p>
 
-                        <div className='flex gap-5 items-center ml-auto'>
-                            <p className='text-xs text-zinc-500 flex items-center gap-2'><TbRotate2 /> Posted 2mins ago</p>
-                            <p className='text-xs text-zinc-500 flex items-center gap-2'><FaEye /> 168 Views</p>
-                            <p className='text-xs text-zinc-500 flex items-center gap-2'><FaRegCalendarAlt /> 58 Scheduled</p>
+                        <div className='flex gap-5 items-center ml-auto max-sm:block max-sm:pt-4'>
+                            <p className='text-xs text-zinc-500 flex items-center gap-2 max-sm:py-1'><TbRotate2 /> Posted 2mins ago</p>
+                            <p className='text-xs text-zinc-500 flex items-center gap-2 max-sm:py-1'><FaEye /> 168 Views</p>
+                            <p className='text-xs text-zinc-500 flex items-center gap-2 max-sm:py-1'><FaRegCalendarAlt /> 58 Scheduled</p>
                         </div>
                     </div>
                 </div>
@@ -114,7 +114,7 @@ const AnnounceBody = () => {
             </div>            
         </div>
 
-        <div className=' bg-white w-100 ml-auto p-10 rounded-3xl'>
+        <div className=' bg-white w-100 ml-auto p-10 rounded-3xl max-sm:px-5 max-sm:w-full'>
 
             <p className='flex items-center gap-4 text-xl font-semibold '><GoDotFill className='text-blue-800'/> Notification</p>
             
